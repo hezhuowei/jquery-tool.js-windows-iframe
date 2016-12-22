f82adbde5e03af03b0334b13041affbe81a6e704
hezhuowei <hezhuowei@126.com>
2016-12-22 14:28:16 +0800

    完成基本功能

---
+++ js/shortcut.js
@@ -0,0 +1,14 @@
+//id,iconName,iconUrl,url,width,height
+var shortcut = [
+[0,"脚本之家","images/icos/logo_hd.png","http://www.jb51.net/",1000,500],
+[1,"酷6电影","images/icos/ku6.png","http://www.jb51.net/",770,500],
+[2,"脚本之家","images/icos/logo_hd.png","http://www.jb51.net/",1000,500],
+[3,"豆瓣FM","images/icos/news.png","http://douban.fm/",1024,500],
+[4,"脚本之家","images/icos/logo_hd.png","http://www.jb51.net/",600,500],
+[5,"脚本之家","images/icos/logo_hd.png","http://www.jb51.net",1000,500],
+[6,"优酷","images/icos/youku.png","http://www.youku.com/",1000,500],
+[7,"脚本之家","images/icos/logo_hd.png","http://www.jb51.net",1000,500],
+[8,"脚本之家","images/icos/son.png","http://www.jb51.net/",740,500],
+[9,"脚本之家","images/icos/bus.png","http://www.jb51.net/",740,500],
+[10,"脚本之家","images/icos/logo_hd.png","http://www.jb51.net",900,540],
+];
